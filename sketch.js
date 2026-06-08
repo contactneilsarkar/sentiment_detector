0eafc0f2c7c0eb8ee78e5607e17f8d5b09b803fb
contactneilsarkar <contactneilsarkar@gmail.com>
2026-06-08 18:56:43 +0000

    Update p5 sentiment analysis project

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//---------------------------Sentiment Analysis Model------
+//Sentiment Analysis model is a model which helps in detecting the sentiment analysis of the text. that means it detects positive negative  neutral sentiments. it has lot of words inside that. The file moviereview has basically the words hat match with movie review words. This was actually to detect the sentiment of the moview rebiew but was later used for ptehr programs too
+
+//
+//-------------------Real life use of sentiment Analysis-
+
+//1. Movie Review ny analyzing the sentimenst of the movie.
+//2. Detecting the sentiments of the text app
+//3. Social media montoring to analyze the sentiments of the user, content
+//4. AI sentiment recognizing
+//goal of project: take input from user, calculate sentiment score, if it is towards hundreds=positive. if it is towards 50=neutral. if it is towards 5=negative.
+let model;
+let result;
+let inputBox;
+let button1;
+
+function setup() {
+  
+  model=ml5.sentiment("movieReviews")
+  inputBox=select("#textBox")
+  button1=select("#inputButton")
+  result=select("#output")
+  button1.mousePressed(calculateScore)
+}
+function calculateScore(){
+  let x=inputBox.value()
+  let prediction=model.predict(x)
+  result.html("sentiment score: "+prediction.score)
+}
